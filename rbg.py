4fa598e2de59304b96893d87eeb30061de832d65
gab_ackles <166563651+gabriel5590@users.noreply.github.com>
2025-11-25 17:55:41 -0300

    Add files via upload

---
+++ rbg.py
@@ -0,0 +1,13 @@
+import math
+import time
+
+
+def rgb(velocidade):
+	t = time.perf_counter() * velocidade
+	r = int((math.sin(t +0 ) * 127 + 128))
+	g = int((math.sin(t + 2) * 127 + 128))
+	b =  int((math.sin(t +4 ) * 127 + 128))
+	return f"0x{r:02X}{g:02X}{b:02X}"
+	
+while True:
+    print(rgb(2.0))
